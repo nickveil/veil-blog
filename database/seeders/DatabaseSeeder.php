ef2e2e09eec6b9d7133f341c73e117f4e1f408e3
nickveil <31191433+nickveil@users.noreply.github.com>
2021-06-02 10:32:00 -0500

    creating a save point

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+                'name' => 'Personal',
+                'slug' => 'personal'
+        ]);
+        
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+        
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        Post::create([ 
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'A short little excerpt',
+            'body' => 'Duis vulputate! Orci eius hic vitae vero malesuada dicta! Numquam similique neque! Parturient ad cupiditate, facilis nostra laboriosam? Voluptates dolores qui maxime justo eos. Litora ac minim harum do pariatur lectus optio asperiores, morbi ante dapibus id ligula odit senectus, bibendum laboris, do malesuada, blandit.'
+
+        ]);
+
+        Post::create([ 
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-second-post',
+            'excerpt' => 'A short little excerpt',
+            'body' => 'Duis vulputate! Orci eius hic vitae vero malesuada dicta! Numquam similique neque! Parturient ad cupiditate, facilis nostra laboriosam? Voluptates dolores qui maxime justo eos. Litora ac minim harum do pariatur lectus optio asperiores, morbi ante dapibus id ligula odit senectus, bibendum laboris, do malesuada, blandit.'
+
+        ]);
+
+        Post::create([ 
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-third-post',
+            'excerpt' => 'A short little excerpt',
+            'body' => 'Duis vulputate! Orci eius hic vitae vero malesuada dicta! Numquam similique neque! Parturient ad cupiditate, facilis nostra laboriosam? Voluptates dolores qui maxime justo eos. Litora ac minim harum do pariatur lectus optio asperiores, morbi ante dapibus id ligula odit senectus, bibendum laboris, do malesuada, blandit.'
+
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +10,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+// | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts', [
+        'posts' => Post::with('category')->get()
+    ]);
+});
+
+Route::get('posts/{post:slug}', function(Post $post){
+    return view('post', [
+        'post'=> $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category){
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
 });
