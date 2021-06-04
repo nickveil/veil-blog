b3923a1397d994a3f6f2f0d845cca5a24989f703
nickveil <31191433+nickveil@users.noreply.github.com>
2021-06-04 14:04:48 -0500

    updated html and css - added compnents

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,20 +2,16 @@
     @include('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card/>
+        <x-post-featured-card :post="$posts[0]"/>
 
         <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>       
+            @foreach ($posts->skip(1) as $post)
+                
+                <x-post-card :post="$post"/>  
+                
+            @endforeach   
         </div>
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card/>
-            <x-post-card/>
-            <x-post-card/>
-
-            
-        </div>
     </main>
     {{-- @foreach ($posts as $post)
         <article>
