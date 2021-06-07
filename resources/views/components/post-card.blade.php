7a9a1f64d2ad780f1f3158d9a3a9dca79d69965e
nickveil <31191433+nickveil@users.noreply.github.com>
2021-06-07 11:25:30 -0500

    created dynamic blog posts and cat button comp

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,7 +1,7 @@
 @props(['post'])
 
 <article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    {{ $attributes->merge(['class'=>'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl']) }}>
     <div class="py-6 px-5">
         <div>
             <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
@@ -10,9 +10,7 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="{{$post->category->slug}}"
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{ $post->category->name}}</a>
+                    <x-category-button :category="$post->category"/>
                 </div>
 
                 <div class="mt-4">
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,33 +2,12 @@
     @include('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]"/>
-
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($posts->skip(1) as $post)
-                
-                <x-post-card :post="$post"/>  
-                
-            @endforeach   
-        </div>
-
-    </main>
-    {{-- @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug}}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name}}</a>
-             </p>
-
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
+        @if ($posts->count())
         
-    @endforeach --}}
+            <x-posts-grid :posts="$posts" />
+            
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
+    </main>
 </x-layout>
