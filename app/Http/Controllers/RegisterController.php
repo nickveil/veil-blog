343a5cfc0e91b3f83cf8fcbb7c2b065443ae44fa
nickveil <31191433+nickveil@users.noreply.github.com>
2021-06-09 17:15:55 -0500

    added registration and login

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
@@ -22,7 +23,9 @@ class RegisterController extends Controller
             'password' => ['required','min:7','max:255'],
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        Auth::login($user);
 
         return redirect('/')->with('success','Your account has been created.');
     }
